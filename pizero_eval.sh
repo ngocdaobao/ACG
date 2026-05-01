0e9ed30158803202974d099e1fba03cc3de3267f
ngocdaobao <dbaongoc2312@gmail.com>
2026-05-01 21:44:04 +0700

    set task

--- pizero_eval.sh
+++ pizero_eval.sh
@@ -6,14 +6,10 @@ policies=("pizero")
 checkpoints=("pretrained/open-pi-zero")
 tasks=(
     "google_robot_pick_coke_can"
-    "google_robot_move_near"
-    "google_robot_close_drawer"
-    "google_robot_open_drawer"
-    "widowx_put_eggplant_in_basket"
-    "widowx_spoon_on_towel"
-    "widowx_carrot_on_plate"
-    "widowx_stack_cube"
-    "google_robot_place_apple_in_closed_top_drawer"
+    # "google_robot_move_near"
+    # "google_robot_close_drawer"
+    # "google_robot_open_drawer"
+    # "google_robot_place_apple_in_closed_top_drawer"
 )
 
 for i in "${!policies[@]}"; do

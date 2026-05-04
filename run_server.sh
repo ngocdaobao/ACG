5c49a1922e052b7f1054e095ab7e550c640f4ab6
Phuc3010 <dbaongoc2312@gmail.com>
2026-05-04 12:24:24 +0200

    edit path

--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 uv run --active --no-sync --directory libs/Isaac_GR00T_n1d6 \
   python -m gr00t.eval.run_gr00t_server \
-  --model-path "gr00tn1.6 checkpoint" \
+  --model-path "nvidia/GR00T-N1.6-3B" \
   --embodiment-tag ROBOCASA_PANDA_OMRON \
   --use-sim-policy-wrapper \
   --use_guidance \
-  --guidance cfg
+  --guidance acg
   
